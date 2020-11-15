8d4a9de9aa24cda1ba36c97fc4a6e0da1b8cf938
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-11-15 20:10:33 -0300

    Resolução Exercício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int numero;
+
+            Console.Write("Digite um Número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("\nVocê digitou um Número negativo.");
+                //Environment.Exit(-1);
+            }
+          //  else
+          //  {
+          //      Console.WriteLine("\nVocê digitou um Número Positivo.");
+          //  }
+            
+
+            Console.WriteLine("\npress enter to finish...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
